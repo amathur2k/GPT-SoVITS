35fc3ad7a2857cc4b588ae68768b6bfc9c6f14ab
abhishek <amathur2k@gmail.com>
2024-07-19 14:50:31 +0530

    Added Calll to batch script

--- install.bat
+++ install.bat
@@ -1,5 +1,10 @@
-conda install -c conda-forge gcc -y
-conda install -c conda-forge gxx -y
-conda install ffmpeg cmake -y
-conda install pytorch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 pytorch-cuda=11.8 -c pytorch -c nvidia -y
-pip install -r requirements.txt -y
+call conda install -c conda-forge gcc -y
+call echo Done 1
+call conda install -c conda-forge gxx -y
+call echo Done 2
+call conda install ffmpeg cmake -y
+call echo Done 3
+call conda install pytorch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 pytorch-cuda=11.8 -c pytorch -c nvidia -y
+call echo Done 4
+call pip install -r requirements.txt
+call echo Done 5
