1b99967e367c9504f48c2e16a88dc3dafa617310
abhishek <amathur2k@gmail.com>
2024-07-20 01:25:58 +0530

    Added trial data and intermediate files

--- finetune.py
+++ finetune.py
@@ -1,1 +1,49 @@
 #This file will be used to seperate out the finetuning codebase
+#Step 1: Slice the Audio
+import os,shutil,sys,pdb,re
+now_dir = os.getcwd()
+sys.path.insert(0, now_dir)
+import json,yaml,warnings,torch
+import platform
+import psutil
+import signal
+
+ps_slice=[]
+
+def open_slice(inp,opt_root,threshold = '-34',min_length = '4000',min_interval = '300',hop_size = '10',
+               max_sil_kept = '500',_max:float = 0.9,alpha:float = 0.25 ,n_parts: int = 4):
+    global ps_slice
+    inp = my_utils.clean_path(inp)
+    opt_root = my_utils.clean_path(opt_root)
+    if(os.path.exists(inp)==False):
+        yield "输入路径不存在",{"__type__":"update","visible":True},{"__type__":"update","visible":False}
+        return
+    if os.path.isfile(inp):n_parts=1
+    elif os.path.isdir(inp):pass
+    else:
+        yield "输入路径存在但既不是文件也不是文件夹",{"__type__":"update","visible":True},{"__type__":"update","visible":False}
+        return
+    if (ps_slice == []):
+        for i_part in range(n_parts):
+            cmd = '"%s" tools/slice_audio.py "%s" "%s" %s %s %s %s %s %s %s %s %s''' % (python_exec,inp, opt_root, threshold, min_length, min_interval, hop_size, max_sil_kept, _max, alpha, i_part, n_parts)
+            print(cmd)
+            p = Popen(cmd, shell=True)
+            ps_slice.append(p)
+        yield "切割执行中", {"__type__": "update", "visible": False}, {"__type__": "update", "visible": True}
+        for p in ps_slice:
+            p.wait()
+        ps_slice=[]
+        yield "切割结束",{"__type__":"update","visible":True},{"__type__":"update","visible":False}
+    else:
+        yield "已有正在进行的切割任务，需先终止才能开启下一次任务", {"__type__": "update", "visible": False}, {"__type__": "update", "visible": True}
+
+def close_slice():
+    global ps_slice
+    if (ps_slice != []):
+        for p_slice in ps_slice:
+            try:
+                kill_process(p_slice.pid)
+            except:
+                traceback.print_exc()
+        ps_slice=[]
+    return "已终止所有切割进程", {"__type__": "update", "visible": True}, {"__type__": "update", "visible": False}
